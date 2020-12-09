f4c14e452af3e3e7a5d0e472857b846bd34d55fe
Magnus Gedda <github@gedda.se>
2020-12-09 22:10:34 +0100

    Cleanup day 8 2020

--- 2020/Day08/Solution.cs
+++ 2020/Day08/Solution.cs
@@ -15,67 +15,23 @@ namespace AdventOfCode.Y2020.Day08
 
         int PartOne(string input)
         {
-            int RunProgram(ImmutableArray<Instruction> program, int i, int accumulator, ImmutableHashSet<int> visited)
-            {
-                if (visited.Contains(i))
-                {
-                    return accumulator;
-                }
-
-                return program[i] switch
-                {
-                        { Code: "acc" } => RunProgram(program, i + 1, accumulator + program[i].Value, visited.Add(i)),
-                        { Code: "jmp" } => RunProgram(program, i + program[i].Value, accumulator, visited.Add(i)),
-                        { Code: "nop" } => RunProgram(program, i + 1, accumulator, visited.Add(i)),
-                        _ => throw new IndexOutOfRangeException()
-                    };
-            }
-
             var program = ParseInput(input);
-            return RunProgram(program, 0, 0, ImmutableHashSet<int>.Empty);
+            return RunProgram(program, 0, 0, ImmutableHashSet<int>.Empty).Item2;
         }
 
         int PartTwo(string input)
         {
-            ImmutableHashSet<ImmutableArray<Instruction>> AlterProgram(ImmutableArray<Instruction> program, int i, ImmutableHashSet<ImmutableArray<Instruction>> programs)
-            {
-                if (i == program.Length)
-                {
-                    return programs;
-                }
-
-                return program[i] switch
-                {
-                    { Code: "nop" } => AlterProgram(program, i + 1, programs.Add(program.SetItem(i, new Instruction { Code = "jmp", Value = program[i].Value }))),
-                    { Code: "jmp" } => AlterProgram(program, i + 1, programs.Add(program.SetItem(i, new Instruction { Code = "nop", Value = 0 } ))),
-                    _ => AlterProgram(program, i + 1, programs)
-                };
-            }
-
-            (int, int) RunProgram(ImmutableArray<Instruction> program, int i, int accumulator, ImmutableHashSet<int> visited)
-            {
-                if (visited.Contains(i) || i == program.Length)
-                {
-                    return (i, accumulator);
-                }
-
-                return program[i] switch
-                {
-                    { Code: "acc" } => RunProgram(program, i + 1, accumulator + program[i].Value, visited.Add(i)),
-                    { Code: "jmp" } => RunProgram(program, i + program[i].Value, accumulator, visited.Add(i)),
-                    { Code: "nop" } => RunProgram(program, i + 1, accumulator, visited.Add(i)),
-                    _ => throw new IndexOutOfRangeException()
-                };
-            }
-
             var program = ParseInput(input);
-            ImmutableHashSet<ImmutableArray<Instruction>> programs = AlterProgram(program, 0, ImmutableHashSet<ImmutableArray<Instruction>>.Empty);
+            var programs = AlterProgram(program, 0, ImmutableHashSet<ImmutableArray<Instruction>>.Empty);
+            return GetAccumulatorForTerminatingProgram(programs);
+        }
 
+        private int GetAccumulatorForTerminatingProgram(ImmutableHashSet<ImmutableArray<Instruction>> programs)
+        {
             foreach (var p in programs)
             {
                 (int i, int accumulator) = RunProgram(p, 0, 0, ImmutableHashSet<int>.Empty);
-                if (i == p.Length)
-                {
+                if (i == p.Length) {
                     return accumulator;
                 }
             }
@@ -83,6 +39,32 @@ namespace AdventOfCode.Y2020.Day08
             throw new IndexOutOfRangeException();
         }
 
+        (int, int) RunProgram(ImmutableArray<Instruction> program, int i, int accumulator, ImmutableHashSet<int> visited)
+        {
+            if (visited.Contains(i) || i == program.Length) {
+                return (i, accumulator);
+            }
+
+            return program[i] switch {
+                { Code: "acc" } => RunProgram(program, i + 1, accumulator + program[i].Value, visited.Add(i)),
+                { Code: "jmp" } => RunProgram(program, i + program[i].Value, accumulator, visited.Add(i)),
+                { Code: "nop" } => RunProgram(program, i + 1, accumulator, visited.Add(i)),
+                _ => throw new IndexOutOfRangeException()
+            };
+        }
+
+        ImmutableHashSet<ImmutableArray<Instruction>> AlterProgram(ImmutableArray<Instruction> program, int i, ImmutableHashSet<ImmutableArray<Instruction>> programs)
+        {
+            if (i == program.Length) {
+                return programs;
+            }
+
+            return program[i] switch {
+                { Code: "nop" } => AlterProgram(program, i + 1, programs.Add(program.SetItem(i, new Instruction { Code = "jmp", Value = program[i].Value }))),
+                { Code: "jmp" } => AlterProgram(program, i + 1, programs.Add(program.SetItem(i, new Instruction { Code = "nop", Value = 0 } ))),
+                _ => AlterProgram(program, i + 1, programs)
+            };
+        }
 
         private ImmutableArray<Instruction> ParseInput(string input)
         {
